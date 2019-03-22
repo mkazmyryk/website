89d166240379533f66e135e8f95d37f9e5c9f690
mkazmyryk <madmax061991@gmail.com>
2019-03-22 13:42:18 +0200

    Feed improvements

--- src/main/java/com/mkaz/website/controller/MainController.java
+++ src/main/java/com/mkaz/website/controller/MainController.java
@@ -1,16 +1,29 @@
 package com.mkaz.website.controller;
 
+import com.mkaz.website.entity.Game;
 import com.mkaz.website.repository.GamesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+import java.util.List;
+
+@Controller
 public class MainController {
     private GamesRepository gamesRepository;
 
     @Autowired
     public MainController(GamesRepository gamesRepository) {
         this.gamesRepository = gamesRepository;
+    }
+
+
+    @GetMapping("/feed")
+    public String gamesForFeed(Model model) {
+        List<Game> gameList = gamesRepository.findAll();
+        model.addAttribute("games", gameList);
+        return "feed";
     }
 
 
