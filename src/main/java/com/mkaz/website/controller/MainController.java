d83c320008990c9f2fa8b2bcc2a57a610125d4fd
mkazmyryk <madmax061991@gmail.com>
2019-04-01 14:49:43 +0300

    Added pagination

--- src/main/java/com/mkaz/website/controller/MainController.java
+++ src/main/java/com/mkaz/website/controller/MainController.java
@@ -3,11 +3,11 @@ package com.mkaz.website.controller;
 import com.mkaz.website.entity.Game;
 import com.mkaz.website.repository.GamesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class MainController {
@@ -21,20 +21,9 @@ public class MainController {
 
     @GetMapping("/feed")
     public String gamesForFeed(Model model) {
-        List<Game> gameList = gamesRepository.findAll();
-        model.addAttribute("games", gameList);
+        Page<Game> page = gamesRepository.findAll(PageRequest.of(0, 2));
+        model.addAttribute("games", page);
         return "feed";
     }
 
-
-//    @GetMapping(value = "/")
-//    public ModelAndView main(ModelAndView modelAndView){
-//        System.out.println("DONE");
-//        modelAndView.setViewName("hello");
-//        Game game = new Game();
-//        game.setTitle("PUBG");
-//        game.setGenre("ACTION");
-//        gamesRepository.save(game);
-//        return modelAndView;
-//    }
 }
--- src/main/java/com/mkaz/website/repository/GamesRepository.java
+++ src/main/java/com/mkaz/website/repository/GamesRepository.java
@@ -1,10 +1,15 @@
 package com.mkaz.website.repository;
 
 import com.mkaz.website.entity.Game;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GamesRepository extends JpaRepository<Game, Long> {
+
+    Page<Game> findAll(Pageable pageable);
+
     Game findByTitle(String title);
 }
