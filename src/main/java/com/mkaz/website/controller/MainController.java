592b56b2849f3745f2f4d0cbf4d1a58b0a4eea51
mkazmyryk <madmax061991@gmail.com>
2019-04-02 09:21:52 +0300

    Pagination improvements

--- src/main/java/com/mkaz/website/controller/MainController.java
+++ src/main/java/com/mkaz/website/controller/MainController.java
@@ -8,6 +8,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class MainController {
@@ -20,9 +26,25 @@ public class MainController {
 
 
     @GetMapping("/feed")
-    public String gamesForFeed(Model model) {
-        Page<Game> page = gamesRepository.findAll(PageRequest.of(0, 2));
-        model.addAttribute("games", page);
+    public String gamesForFeed(Model model,
+                               @RequestParam("page") Optional<Integer> page,
+                               @RequestParam("size") Optional<Integer> size) {
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(5);
+
+        Page<Game> games = gamesRepository.findAll(PageRequest.of(currentPage - 1, pageSize));
+        model.addAttribute("games", games);
+
+        int totalPages = ((int) (gamesRepository.count() / pageSize)) + 1;
+
+
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
         return "feed";
     }
 
