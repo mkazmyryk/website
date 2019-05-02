2a42ffbbbb4406e82569254a58cc07c33581d233
mkazmyryk <madmax061991@gmail.com>
2019-05-02 08:37:44 +0300

    ROLE_ADMIN is default for all users now.

--- src/main/java/com/mkaz/website/controller/RegistrationController.java
+++ src/main/java/com/mkaz/website/controller/RegistrationController.java
@@ -36,8 +36,16 @@ public class RegistrationController {
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         user.setEnabled(true);
         Role role = rolesRepository.findByName("ROLE_ADMIN");
-        user.getRoles().add(role);
-        role.getUsers().add(user);
+        if (role != null) {
+            user.getRoles().add(role);
+            role.getUsers().add(user);
+        } else {
+            role = new Role();
+            role.setName("ROLE_ADMIN");
+            role.getUsers().add(user);
+            user.getRoles().add(role);
+        }
+        rolesRepository.save(role);
         userRepository.save(user);
         return "redirect:/login";
     }
